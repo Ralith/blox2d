ad7f6322879a49c0b05ca7830af7443ecf06019b
Ralith <ralith@gmail.com>
2009-07-26 16:41:41 -0700

    Added manifold-point

--- collision/collision.lisp
+++ collision/collision.lisp
@@ -6,6 +6,12 @@
   (incident-vertex nil :type (signed-byte 8))
   (flip nil :type (signed-byte 8)))
 
+(defstruct manifold-point
+  (local-point nil :type vec2)
+  (normal-impulse nil :type double-float)
+  (tangent-impulse nil :type double-float)
+  (id nil :type contact-id))
+
 (defstruct aabb
-  (lower-bound nil :type vector)
-  (upper-bound nil :type vector))
+  (lower-bound nil :type vec2)
+  (upper-bound nil :type vec2))
