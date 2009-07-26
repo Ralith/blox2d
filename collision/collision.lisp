a853928dbf596f046f25bff3d4a0b87b0214342f
Ralith <ralith@gmail.com>
2009-07-26 16:48:13 -0700

    Added manifold

--- collision/collision.lisp
+++ collision/collision.lisp
@@ -12,6 +12,13 @@
   (tangent-impulse nil :type double-float)
   (id nil :type contact-id))
 
+(defstruct manifold
+  (points nil :type (simple-vector 2))  ;b2_maxManifoldPoints = 2
+  (local-plane-normal nil :type vec2)
+  (local-point nil :type vec2)
+  (type nil :type symbol)
+  (point-count nil :type (signed-byte 32)))
+
 (defstruct aabb
   (lower-bound nil :type vec2)
   (upper-bound nil :type vec2))
