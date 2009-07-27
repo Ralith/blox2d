be7c3d704f45edfe8a52cb42107d532a93e10c42
Ralith <ralith@gmail.com>
2009-07-26 17:44:26 -0700

    Really fixed types and initial values this time.

---
+++ common/math.lisp
@@ -0,0 +1,18 @@
+(in-package :blox2d)
+
+(deftype vec2 ()
+  '(simple-array single-float (2)))
+
+(defun vec2 (x y)
+  (make-array 2
+              :element-type 'single-float
+              :initial-contents (list x y)))
+
+(defun vec2-x (vec)
+  (elt vec 0))
+
+(defun vec2-y (vec)
+  (elt vec 1))
+
+(defconstant +zero-vec2+ (vec2 0.0 0.0)
+  "The zero vector.")
